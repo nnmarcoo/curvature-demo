fdcc77301f6075360d35b269fc16cd39ba856844
Marco <marcosteam7@gmail.com>
2024-06-22 12:53:17 -0400

    Fixed bezier function

--- main.js
+++ main.js
@@ -201,16 +201,14 @@ window.addEventListener('DOMContentLoaded', async () => {
 }
 
   function cubicBezier(p0, p1, p2, p3, t) {
-    let x =
-      Math.pow(1 - t, 3) * p0[0] +
-      3 * Math.pow(1 - t, 2) * p1[0] +
-      3 * Math.pow(1 - t, 2) * Math.pow(t,2) * p2[0] +
-      Math.pow(t,3) * p3[0];
-    let y =
-      Math.pow(1 - t, 3) * p0[1] +
-      3 * Math.pow(1 - t, 2) * p1[1] +
-      3 * Math.pow(1 - t, 2) * Math.pow(t,2) * p2[1] +
-      Math.pow(t,3) * p3[1];
+    const x = Math.pow(1 - t, 3) * p0[0] +
+              3 * Math.pow(1 - t, 2) * t * p1[0] +
+              3 * (1 - t) * Math.pow(t, 2) * p2[0] +
+              Math.pow(t, 3) * p3[0];
+    const y = Math.pow(1 - t, 3) * p0[1] +
+              3 * Math.pow(1 - t, 2) * t * p1[1] +
+              3 * (1 - t) * Math.pow(t, 2) * p2[1] +
+              Math.pow(t, 3) * p3[1];
     return [x, y];
   }
 
